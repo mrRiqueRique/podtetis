8a7b5631cf4cc58e5f1f93c6124858d16cbea11b
mrRiqueRique <riquemribeiro2005@gmail.com>
2026-03-08 16:23:33 -0300

    start

--- estela.py
+++ estela.py
@@ -21,16 +21,25 @@ service = build("sheets", "v4", credentials=creds)
 sheet = service.spreadsheets()
 
 SPREADSHEET_ID = "1QCmmgq-d9DNWk6w2hI-FgYVHZ8qveAuov44qUiGTkYs"
-RANGE = "Página1!A1:C10"
 
+#inicializar objetos
 
-# inicializar objetos
 driver = webdriver.Chrome()
 driver.get("https://podcastcharts.byspotify.com/br")
+
 wait = WebDriverWait(driver, 10)
 
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(1)
+# seleciona somente os podcasts da categoria ciência
+dropdown = driver.find_element(By.ID, "categoryDropdown")
+dropdown.click()
+buttons = driver.find_elements(By.CSS_SELECTOR, ".relative.transition-all")
+for button in buttons:
+    if button.text == "Ciências":
+        ciencia = button    
+    
+ciencia.click()
+
+time.sleep(2)
 
 list_container = wait.until(
     expected_conditions.presence_of_element_located(
@@ -40,21 +49,77 @@ list_container = wait.until(
 
 items = list_container.find_elements(By.CSS_SELECTOR, ".Show_show__default__2x1b_")
 
-top = []
+top = [[], []]
 for item in items:
     text = item.text.split('\n')
-    podcast = [text[2], text[3]]
-    top.append(podcast)
+    top[0].append(text[2])
+    top[1].append(text[3])
 
 driver.quit()
 
-body = {
-    "values": top
-}
+currentDay = int(
+    sheet.values().get(
+        spreadsheetId=SPREADSHEET_ID,
+        range="config!B1"
+    ).execute().get("values", [])[0][0]
+) + 1
+
+
+currentDay = int(currentDay)
+sheet.values().update(
+    spreadsheetId=SPREADSHEET_ID,
+    range="config!B1",
+    valueInputOption="RAW",
+    body = {
+    "values": [
+        [str(currentDay)]
+        ]
+    }
+).execute()
+
+# Atualiza planilha na posição adequada
+sheet.values().update(
+    spreadsheetId=SPREADSHEET_ID,
+    range=f"Página1!A{(currentDay*2) - 1}",
+    valueInputOption="RAW",
+    body={
+        "values": top
+    }
+).execute()
+
+# AGORA PRO DICIONARIO
+podcasts = dict(
+    sheet.values().get(
+        spreadsheetId=SPREADSHEET_ID,
+        range="info!A:B"
+    ).execute()["values"]
+)
+
+for podcast in top[0]:
+    if podcast in podcasts:
+        podcasts[podcast] = int(podcasts[podcast]) + 1
+    else:
+        podcasts[podcast] = 1
+
+# atualizar values
+vezes = []
+for podcast in podcasts:
+    vezes.append([podcast, podcasts[podcast]])
 
 sheet.values().update(
     spreadsheetId=SPREADSHEET_ID,
-    range="Página1!A1",
+    range="info!A1",
     valueInputOption="RAW",
-    body=body
+    body={
+        "values": vezes
+    }
 ).execute()
+
+
+
+# vezes = sheet.values().get(
+#     spreadsheetId=SPREADSHEET_ID,
+#     range="info!B:B"
+# ).execute()["values"]
+
+# print(vezes)
